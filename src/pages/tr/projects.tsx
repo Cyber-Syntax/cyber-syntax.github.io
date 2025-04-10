05623cb389df159d027870b0f1a8bbbe2d958563
Cyber-Syntax <115875369+Cyber-Syntax@users.noreply.github.com>
2025-04-10 14:02:11 +0300

    chore: remove breadcrumb navigation from pages

--- src/pages/tr/projects.tsx
+++ src/pages/tr/projects.tsx
@@ -149,38 +149,6 @@ function ProjectPage() {
 
   return (
     <Layout title="Projeler" description="Projelerim">
-      <nav
-        className="theme-doc-breadcrumbs breadcrumbsContainer_Z_bl"
-        aria-label="Breadcrumbs"
-      >
-        <ul
-          className="breadcrumbs padding--lg"
-          itemScope={true}
-          itemType="https://schema.org/BreadcrumbList"
-        >
-          <li className="breadcrumbs__item">
-            <a aria-label="Ana sayfa" className="breadcrumbs__link" href="/">
-              <svg viewBox="0 0 24 24" className="breadcrumbHomeIcon_YNFT">
-                <path
-                  d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </a>
-          </li>
-          <li
-            itemScope={true}
-            itemProp="itemListElement"
-            itemType="https://schema.org/ListItem"
-            className="breadcrumbs__item breadcrumbs__item--active"
-          >
-            <span className="breadcrumbs__link" itemProp="name">
-              Projeler
-            </span>
-            <meta itemProp="position" content="1" />
-          </li>
-        </ul>
-      </nav>
 
       <main className="container">
         <section className="portfolio-section">
